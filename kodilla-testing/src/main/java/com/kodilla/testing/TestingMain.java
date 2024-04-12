5b91fe83f291f18b56803b9881fd8d9bb3fc195c
Marcel Gad <marcelgad@gmail.com>
2024-04-12 04:00:54 +0200

    First commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        testCalculator();
+    }
+
+    public static void testCalculator() {
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(5, 3);
+        if (addResult == 8) {
+            System.out.println("Test for add() passed.");
+        } else {
+            System.out.println("Test for add() failed.");
+        }
+
+        int subtractResult = calculator.subtract(5, 3);
+        if (subtractResult == 2) {
+            System.out.println("Test for subtract() passed.");
+        } else {
+            System.out.println("Test for subtract() failed.");
+        }
     }
 }
