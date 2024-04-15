206311abab5116129340dff0aee9022afe9fda40
Marcel Gad <marcelgad@gmail.com>
2024-04-15 13:13:39 +0200

    First Commit

--- kodilla-testing/src/test/java/com/kodilla/testing/shape/shapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/shapeCollectorTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class shapeCollectorTestSuite {
+class ShapeCollectorTestSuite {
 
     @Nested
     @DisplayName("Tests for ShapeCollector class")
