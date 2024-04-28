363c3637299bf1a785621484315bba4151dbe085
Marcel Gad <marcelgad@gmail.com>
2024-04-28 20:58:44 +0200

    first commit

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,37 +1,12 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier beautifier = new PoemBeautifier();
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
-        beautifier.beautify("Sample text", text -> "ABC" + text + "ABC");
-
-        beautifier.beautify("Another sample text", String::toUpperCase);
-
-        // Własny upiększacz: zamiana każdej litery na jej odpowiednik w alfabecie Morse'a
-        beautifier.beautify("Hello World", text -> {
-            StringBuilder morseText = new StringBuilder();
-            for (char c : text.toCharArray()) {
-                switch (Character.toLowerCase(c)) {
-                    case 'a':
-                        morseText.append(".- ");
-                        break;
-                    case 'b':
-                        morseText.append("-... ");
-                        break;
-                    // Pozostałe przypadki alfabetu Morse'a
-
-                    default:
-                        morseText.append(c).append(" ");
-                        break;
-                }
-            }
-            return morseText.toString();
-        });
-
-        // Własny upiększacz: odwrócenie kolejności znaków w tekście
-        beautifier.beautify("Reverse me", text -> new StringBuilder(text).reverse().toString());
     }
 }
