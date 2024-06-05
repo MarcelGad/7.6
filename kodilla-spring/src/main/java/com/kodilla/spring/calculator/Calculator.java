6cebbee85c945483493b6f252774f6264dec00b9
Marcel Gad <marcelgad@gmail.com>
2024-06-05 22:45:11 +0200

    zadanie 10.2

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -13,18 +13,26 @@ public class Calculator {
     }
 
     public double add(double a, double b) {
-        return a + b;
+        double result = a + b;
+        display.displayValue(result);
+        return result;
     }
 
     public double sub(double a, double b) {
-        return a - b;
+        double result = a - b;
+        display.displayValue(result);
+        return result;
     }
 
     public double mul(double a, double b) {
-        return a * b;
+        double result = a * b;
+        display.displayValue(result);
+        return result;
     }
 
     public double div(double a, double b) {
-        return a / b;
+        double result = a / b;
+        display.displayValue(result);
+        return result;
     }
 }
