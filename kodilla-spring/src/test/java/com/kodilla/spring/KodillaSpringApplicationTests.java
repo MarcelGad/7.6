af4bef44854f35c536e6dc624770993ae02d15e6
Marcel Gad <marcelgad@gmail.com>
2024-06-05 03:11:49 +0200

    zadanie 10.3

--- kodilla-spring/src/test/java/com/kodilla/spring/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/KodillaSpringApplicationTests.java
@@ -5,11 +5,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-class KodillaApplicationTests {
+@SpringBootApplication
+class KodillaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaSpringApplication.class, args);
+    }
 
     @Test
     void testCircleLoadedIntoContainer() {
